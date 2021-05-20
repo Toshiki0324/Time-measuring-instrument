fc48da24defd6b3bfa0093fdb0fe90753189ddf8
Toshiki0324 <67587427+Toshiki0324@users.noreply.github.com>
2021-05-20 18:04:27 +0900

    Update install-tf-v45.sh

--- install-tf-v45.sh
+++ install-tf-v45.sh
@@ -15,5 +15,7 @@ sudo apt-get -y install zlib1g-dev zip libjpeg8-dev libhdf5-dev
 sudo pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 sudo pip3 install 
 
+sudo apt install mosquitto
+
 # Install TensorFlow
 sudo pip3 install --no-cache-dir --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 'tensorflow<2'
